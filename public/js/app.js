acde45bf93c961e7622241a5ae4f245f75b760d7
JeffBaucom <jeffbaucom@gmail.com>
2015-07-30 17:08:20 -0700

    Added Forms for Testing Front-End

--- public/js/app.js
+++ public/js/app.js
@@ -13,4 +13,13 @@ angular.module('myApp', ['ngMap'])
 	{lat : '37.772953', lng:'-122.419598', title: 'GroovMekanex HQ'},
 	{lat : '37.807780', lng:'-122.272723', title: 'thePeople Party'}];
 
+	vm.addMarker = function() {
+		vm.list.push({
+			lat: vm.newLat, lng: vm.newLng, title: vm.newTitle
+		});
+		vm.newLat = "";
+		vm.newLng = "";
+		vm.newTitle = "";
+	}
+
 });
