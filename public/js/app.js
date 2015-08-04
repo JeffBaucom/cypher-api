2d3ab3518ea935691ab50b5a27545395342f1a3f
JeffBaucom <jeffbaucom@gmail.com>
2015-08-03 23:27:55 -0700

    Changed to Address input, added nonfunctional get center button

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,34 @@
 //ngMap dependency for Angular maps directives
 angular.module('myApp', ['ngMap'])
 
-.controller('mainController', function() {
+.controller('mainController', function($scope) {
 	var vm = this;
 
 	vm.message = 'hello';
 
 	vm.pos = 'Georgia Institute of Technology';
 
+	vm.center = [37.853843, -122.278776];
+	var map = $scope.map;
+
 	vm.list = [
 	{lat : '37.853843', lng:'-122.278776', title: 'Open House Bay Area'},
 	{lat : '37.772953', lng:'-122.419598', title: 'GroovMekanex HQ'},
 	{lat : '37.807780', lng:'-122.272723', title: 'thePeople Party'}];
 
+	vm.newList = [];
+
 	vm.addMarker = function() {
-		vm.list.push({
-			lat: vm.newLat, lng: vm.newLng, title: vm.newTitle
+		vm.newList.push({
+			pos:vm.newAddress, title: vm.newTitle
 		});
-		vm.newLat = "";
-		vm.newLng = "";
+		$scope.map.setCenter(vm.newAddress);
+		vm.newAddress = "";
 		vm.newTitle = "";
+	}
+
+	$scope.getCenter = function() {
+		return $scope.map.getCenter();
 	}
 
 });
