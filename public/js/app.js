20d53400bc75848a3a26e6b7c7b09d956a584adf
JeffBaucom <jeffbaucom@gmail.com>
2015-08-04 21:26:49 -0700

    Fixed controller as issue for accessing ma

--- public/js/app.js
+++ public/js/app.js
@@ -4,12 +4,15 @@ angular.module('myApp', ['ngMap'])
 .controller('mainController', function($scope) {
 	var vm = this;
 
+	$scope.$on('mapInitialized', function(evt, map) {
+		vm.map = map;
+	});
+
 	vm.message = 'hello';
 
 	vm.pos = 'Georgia Institute of Technology';
 
 	vm.center = [37.853843, -122.278776];
-	var map = $scope.map;
 
 	vm.list = [
 	{lat : '37.853843', lng:'-122.278776', title: 'Open House Bay Area'},
@@ -22,13 +25,13 @@ angular.module('myApp', ['ngMap'])
 		vm.newList.push({
 			pos:vm.newAddress, title: vm.newTitle
 		});
-		$scope.map.setCenter(vm.newAddress);
 		vm.newAddress = "";
 		vm.newTitle = "";
 	}
 
-	$scope.getCenter = function() {
-		return $scope.map.getCenter();
+	vm.getCenter = function() {
+		console.log(vm.map.getCenter());
+		return vm.map.getCenter();
 	}
 
 });
